2d627b0a8bf6c4707ede5b3223239761786f2d0a
Dimas Camillo <72895101+dimascamillo@users.noreply.github.com>
2024-01-05 11:55:10 -0300

    Finalizada a Sessao Nossa Missao

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-function swapClasses(activeButtonId, activeSectionId, disabledButtonId, disabledSectionId) {
+function swapClassesHowItWork(activeButtonId, activeSectionId, disabledButtonId, disabledSectionId) {
   const activeButton = document.getElementById(activeButtonId);
   const activeSection = document.getElementById(activeSectionId);
   const disabledButton = document.getElementById(disabledButtonId);
@@ -16,9 +16,58 @@ function swapClasses(activeButtonId, activeSectionId, disabledButtonId, disabled
 }
 
 document.getElementById("btn-howItWorks-one").addEventListener("click", function () {
-  swapClasses("btn-howItWorks-one", "card-howItWorks-one", "btn-howItWorks-two", "card-howItWorks-two");
+  swapClassesHowItWork("btn-howItWorks-one", "card-howItWorks-one", "btn-howItWorks-two", "card-howItWorks-two");
 });
 
 document.getElementById("btn-howItWorks-two").addEventListener("click", function () {
-  swapClasses("btn-howItWorks-two", "card-howItWorks-two", "btn-howItWorks-one", "card-howItWorks-one");
+  swapClassesHowItWork("btn-howItWorks-two", "card-howItWorks-two", "btn-howItWorks-one", "card-howItWorks-one");
+});
+
+function swapClassesOurMission(activeButtonId, activeSectionId, disabledButtonIds, disabledSectionIds) {
+  const activeButton = document.getElementById(activeButtonId);
+  const activeSection = document.getElementById(activeSectionId);
+
+  activeButton.classList.add("active");
+  activeButton.classList.remove("disabled");
+  activeSection.classList.add("active");
+  activeSection.classList.remove("disabled");
+
+  disabledButtonIds.forEach((id) => {
+    const button = document.getElementById(id);
+    button.classList.add("disabled");
+    button.classList.remove("active");
+  });
+
+  disabledSectionIds.forEach((id) => {
+    const section = document.getElementById(id);
+    section.classList.add("disabled");
+    section.classList.remove("active");
+  });
+}
+
+document.getElementById("btn-ourMission-one").addEventListener("click", function () {
+  swapClassesOurMission(
+    "btn-ourMission-one",
+    "card-ourMission-one",
+    ["btn-ourMission-two", "btn-ourMission-three"],
+    ["card-ourMission-two", "card-ourMission-three"],
+  );
+});
+
+document.getElementById("btn-ourMission-two").addEventListener("click", function () {
+  swapClassesOurMission(
+    "btn-ourMission-two",
+    "card-ourMission-two",
+    ["btn-ourMission-one", "btn-ourMission-three"],
+    ["card-ourMission-one", "card-ourMission-three"],
+  );
+});
+
+document.getElementById("btn-ourMission-three").addEventListener("click", function () {
+  swapClassesOurMission(
+    "btn-ourMission-three",
+    "card-ourMission-three",
+    ["btn-ourMission-one", "btn-ourMission-two"],
+    ["card-ourMission-one", "card-ourMission-two"],
+  );
 });
